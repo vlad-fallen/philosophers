945af8dcfc40e34c94fde5043942ee0a53eebc5f
Mudcrab Butter <mbutter@il-f1.msk.21-school.ru>
2022-03-07 18:25:10 +0300

    philo mandatory

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: mbutter <mbutter@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:18:56 by mbutter           #+#    #+#             */
-/*   Updated: 2022/03/06 17:42:34 by mbutter          ###   ########.fr       */
+/*   Updated: 2022/03/07 15:20:39 by mbutter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,24 @@ int	create_threads(t_global *glob, t_philo *philo, t_death *death_philo)
 	pthread_t	thread_death;
 
 	threads_id = (pthread_t *)malloc(sizeof(pthread_t) * glob->num_philo);
-	i = 0;
+	i = -1;
 	if (pthread_create(&thread_death, NULL, check_death, death_philo))
 		return (1);
-	while (i < glob->num_philo)
+	while (++i < glob->num_philo)
 	{
 		if (pthread_create(&(threads_id[i]), NULL,
 				philo_routine, &(philo[i])) != 0)
 			return (1);
-		i++;
 	}
-	i = 0;
+	i = -1;
 	if (pthread_join(thread_death, NULL))
 		return (1);
-	while (i < glob->num_philo)
+	while (++i < glob->num_philo)
 	{
 		if (pthread_join(threads_id[i], NULL) != 0)
 			return (1);
-		i++;
 	}
+	free(threads_id);
 	return (0);
 }
 
@@ -46,8 +45,9 @@ void	destroy_all(t_philo *philo, t_global *glob)
 	int	i;
 
 	i = 0;
+	pthread_mutex_destroy(philo->print_mutex);
 	while (i < glob->num_philo)
-		pthread_mutex_destroy(philo[i].right_fork->fork);
+		pthread_mutex_destroy(philo[i++].right_fork->fork);
 	free(philo->right_fork->fork);
 	free(philo->right_fork);
 	free(philo->print_mutex);
@@ -67,4 +67,5 @@ int	main(int argc, char **argv)
 		return (ft_exit("Error with initialization\n", 1));
 	if (create_threads(&glob, philo, &death_philo))
 		return (ft_exit("Error with threads\n", 1));
+	destroy_all(philo, &glob);
 }
